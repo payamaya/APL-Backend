f5e738e8d273f5f0ee50fd9603a4d7e983ba07a6
payamaya <paulyashou81@gmail.com>
2025-04-08 19:40:57 +0200

    Structured all the folders and files , need some adjustment later for more organaized Reposaitory pattern. And connected the back end with the postgreSQL. Adding a .gitignore as well

---
+++ Application/DTOs/CourseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty; // ⛔ Remove `internal set`
+        public string Description { get; set; } = string.Empty; // ⛔ Remove `internal set`
+    }
+
+}
