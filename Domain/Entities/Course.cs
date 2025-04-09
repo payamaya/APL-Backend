b973f5894ff5745020583cf02eec184cea9d62f9
payamaya <paulyashou81@gmail.com>
2025-04-09 16:11:16 +0200

    Change int ot Guid

--- Domain/Entities/Course.cs
+++ Domain/Entities/Course.cs
@@ -8,7 +8,11 @@ namespace Domain.Entities
 {
     public class Course
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
+  
+        public string Title { get; set; } = string.Empty; 
+        public string Description { get; set; } = string.Empty; 
+
     }
 }
