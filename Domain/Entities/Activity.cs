2076ad61d23a5fc4804517dc2cd27cd5b15991c8
mhdkhavari <mhd.kh320@gmail.com>
2025-04-15 12:08:30 +0200

    Created Activity entity with proper attributes and connection relations.

--- Domain/Entities/Activity.cs
+++ Domain/Entities/Activity.cs
@@ -6,17 +6,13 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Module
+    public class Activity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
-
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-
-        public Guid CourseId { get; set; }
-        public Course Course { get; set; }
-        public ICollection<Activity> Activities { get; set; } = new List<Activity>();
-
+        public Guid ModuleId { get; set; }
+        public Module Module { get; set; }
     }
 }
