84517ca9bee05698b7eace7ae3e5feb983ce709f
mhdkhavari <mhd.kh320@gmail.com>
2025-04-15 16:01:16 +0200

    Created an IActivityService file with moduleId as foreign key and activityId as primary key!

---
+++ Application/Interfaces/IActivityService.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IActivityService
+    {
+        Task<IEnumerable<ActivityDto>> GetAllActivityAsync(Guid moduleId);
+        Task<ActivityDto?> GetActivityByIdAsync(Guid moduleId, Guid activityId);
+        Task<ActivityDto> CreateActivityAsync(ActivityDto dto);
+        Task<ActivityDto> UpdateActivityAsync(ActivityDto dto);
+        Task<bool> DeleteActivityAsync(Guid activityId);
+    }
+}
