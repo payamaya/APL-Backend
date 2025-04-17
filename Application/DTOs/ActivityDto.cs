3113245ea5ae5e117fe7bad160666ede1e8316ea
mhdkhavari <mhd.kh320@gmail.com>
2025-04-17 12:14:48 +0200

    Added DueDate and CreatedAt properties to the Activitytype!

--- Application/DTOs/ActivityDto.cs
+++ Application/DTOs/ActivityDto.cs
@@ -16,6 +16,10 @@ namespace Application.DTOs
         public string Description { get; set; } = string.Empty; // ⛔ Remove `internal set`
         public ActivityType ActivityType { get; set; } // ⛔ Remove `internal set`
         public Guid ModuleId { get; set; } // Required foreign key
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public DateTimeOffset? DueDate { get; set; }
     }
 
 }
