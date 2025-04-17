ac5496b5717fa384a1607a4c7c0cfda88a7db308
mhdkhavari <mhd.kh320@gmail.com>
2025-04-17 16:13:06 +0200

    Added Comment entity, Comment Dto an automapper for commentDto!

---
+++ Application/DTOs/CommentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public Guid ActivityId { get; set; } // Required foreign key
+        public Guid UserId { get; set; } // Required foreign key
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; } // Optional for edits
+    }
+}
